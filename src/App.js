4d174d0561832e3999b44cde1f424bf4a84621c8
Somaye <Somaye.tajali@gmail.com>
2021-12-21 11:56:32 +0330

    Project Comitted

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+
+export default function App () {
+
+
+        return (
+            <div>
+                {console.log(age)}
+            </div>
+        );
+}
+
---
+++ src/Blog.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Blog = () => {
+    return <h1>Blog Articles</h1>;
+};
+
+export default Blog;
